838ea144c145fe1420bb8c5c40952713109c6d02
adammkeane <adamkeane.edu@gmail.com>
2023-02-22 09:34:49 +0000

    Set up elephant sql database and basic heroku app

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import os
+import dj_database_url
+import env
 
 if os.path.isfile("env.py"):
     import env
@@ -80,11 +83,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
